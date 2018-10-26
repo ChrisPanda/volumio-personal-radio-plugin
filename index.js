652a5b4ca06fb4b024a5ddc65ae6b5aaef74620d
ChungGeun Song <ckchrisong@gmail.com>
2018-10-26 13:31:34 +0900

    fix KBS radio streaming stop playing irregularly

--- index.js
+++ index.js
@@ -353,12 +353,22 @@ ControllerPersonalRadio.prototype.explodeUri = function (uri) {
       var radioChannel = self.radioStations.kbs[channel].channel;
       self.getStreamUrl(station, self.baseKbsStreamUrl + radioChannel, "")
         .then(function (responseUrl) {
-          if (responseUrl  !== null) {
-            response["uri"] = responseUrl.channel_item[0].service_url;
-            response["name"] = self.radioStations.kbs[channel].title;
-            response["title"] = self.radioStations.kbs[channel].title;
+          if (responseUrl !== null) {
+            var streamingUrl = responseUrl.channel_item[0].service_url;
+            var serverUrl = streamingUrl.split('?')[0];
+            var newUrl =  serverUrl.substring(0, serverUrl.indexOf("play"));
+
+            self.getStreamUrl(station, streamingUrl, "")
+            .then(function (response) {
+              var newQuery = response.substring(response.indexOf("chunk"))
+
+              response["uri"] = newUrl + newQuery;
+              response["name"] = self.radioStations.kbs[channel].title;
+              response["title"] = self.radioStations.kbs[channel].title;
+
+              defer.resolve(response);
+            });
           }
-          defer.resolve(response);
         });
       break;
 
